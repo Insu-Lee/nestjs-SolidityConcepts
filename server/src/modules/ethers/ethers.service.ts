31c6e86bcbde772fb8c38df99e4cac8325afaadc
Insu-Lee <insu.lee@mrhi.or.kr>
2025-05-20 09:19:56 +0900

    reference

--- server/src/modules/ethers/ethers.service.ts
+++ server/src/modules/ethers/ethers.service.ts
@@ -55,22 +55,27 @@ export class EthersService {
 
   async owner() {
     // Todo: owner의 값을 리턴합니다.
+    return await this.contract.owner();
   }
 
   async fixedValue() {
     // Todo: FIXED_VALUE의 값을 리턴합니다.
+    return await this.contract.FIXED_VALUE();
   }
 
   async value() {
     // Todo: value의 값을 리턴합니다.
+    return await this.contract.value();
   }
 
   async checkValue(value: number) {
     // Todo: checkValue의 값을 리턴합니다.
+    return await this.contract.checkValue(value);
   }
 
   async sumUpTo(value: number) {
     // Todo: sumUpTo의값을 리턴합니다.
+    return await this.contract.sumUpTo(value);
   }
 
   async updateValue(value: number) {
@@ -88,29 +93,77 @@ export class EthersService {
         newValue: 1
       }
     */
+
+    const tx = await this.contract.updateValue(value);
+    const receipt = await tx.wait();
+
+    // console.log(receipt);
+
+    for (const log of receipt.logs) {
+      const logDescription = this.contract.interface.parseLog(
+        log
+      ) as LogDescription;
+
+      // console.log(logDescription);
+      if (logDescription.fragment.name === 'ValueChanged') {
+        const oldValue = logDescription.args[0];
+        const newValue = logDescription.args[1];
+        // console.log(oldValue, newValue);
+        result.oldValue = oldValue;
+        result.newValue = newValue;
+      }
+    }
+
+    // console.log(result);
+    return result;
   }
 
   async ownerFunction() {
     // Todo: ownerFunction의값을 리턴합니다.
+
+    return await this.contract.ownerFunction();
   }
 
   async sendEther(address: string, value: number) {
     // Todo: sendEther의값을 리턴합니다.
     // ⚠️ setter함수는 tx 확정 후 영수증을 리턴합니다.(wait)
+
+    const tx = await this.contract.sendEther(address, {
+      value: this.parseEther(value.toString()),
+    });
+    const receipt = await tx.wait();
+
+    return receipt;
   }
 
   async getContractBalance() {
     // Todo: getContractBalance의 값을 리턴합니다.
     // ⚠️ 리턴은 ether 단위로 리턴합니다.(wei => ether)
+
+    const lawBalance = await this.contract.getContractBalance();
+
+    // console.log(this.formatEther(lawBalance));
+    return this.formatEther(lawBalance);
   }
 
   async deposit(value: number) {
-    // Todo: Contract에 코인을 전송합니다.
+    // Todo: Contract에 코인을 전송합니다. (컨트랙트에 직접 코인을 전송해야함)
     // ⚠️ tx 확정 후 영수증을 리턴합니다.(wait)
+
+    const tx = await this.signer.sendTransaction({
+      to: this.contract.target,
+      value: this.parseEther(value.toString()),
+    });
+    const receipt = await tx.wait();
+    return receipt;
   }
 
   async withDraw() {
     // Todo: withDraw의값을 리턴합니다.
     // ⚠️ setter함수는 tx 확정 후 영수증을 리턴합니다.(wait)
+
+    const tx = await this.contract.withDraw();
+    const receipt = await tx.wait();
+    return receipt;
   }
 }
